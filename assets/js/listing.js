cc5b28d60290596e0b5fb8d43ff3feabc233c1a1
Daria <daria.z@mail.ru>
2017-11-23 19:09:37 +0200

    correct indentation

--- assets/js/listing.js
+++ assets/js/listing.js
@@ -1,39 +1,54 @@
 var app = new Vue({
     el: '#UserList',
     data:{
-     users:  Persons
+     users:Persons,
+        pages:[],
+        personPerPage:20,
     },
     methods: {
       remove (index) {     
         this.$delete(this.names, index)
-      },
+        },
         edit (name){
          document.location.href = "https://www.google.com/search?q=" + name;
-    },
+        },
         sortByNumber(){            
             if (this.sorted===false){                
                 this.sorted=true;                  
-          this.names.sort(function (a,b) {              return a.count>b.count;               
+          this.names.sort(function (a,b) {
+              return a.count>b.count;               
             } 
         )}
          else if (this.sorted===true){           
                 this.sorted=false;               
-          this.names.sort(function (a,b) {              return a.count<b.count;//                
+          this.names.sort(function (a,b) {
+              return a.count<b.count;//                
             })}         
         },//sorted
         sortByName(){            
             if (this.sorted===false){                
                 this.sorted=true;                  
-          this.names.sort(function (a,b) {              return a.text.toUpperCase()>b.text.toUpperCase(); } 
+          this.names.sort(function (a,b) {
+              return a.text.toUpperCase()>b.text.toUpperCase(); } 
         )}
          else if (this.sorted===true){           
                 this.sorted=false;               
-          this.names.sort(function (a,b) {              return a.text.toUpperCase()<b.text.toUpperCase();//                
-            })}         
-       
+          this.names.sort(function (a,b) {
+              return a.text.toUpperCase()<b.text.toUpperCase();
+            })} 
+        },
+        computed:{
+            numberPage:function(){
+               return Math.ceil(Persons.length/personPerPage)
+            }
+//            for (i=1; i<numberPage; i++){
+//            pages.push(i);
+//        }
         }
     }//methods
 })//Vue
+
+
 function pagination(num,limit,range)
 
 {   range = range||3;
@@ -49,4 +64,4 @@ function pagination(num,limit,range)
    }
   return arr
 }
-//alert(pagination(5,20)+"\n"+pagination(7,20)+"\n"+pagination(16,20));
+
