05194d30d72e14f31ded0ef0a70bcda2f16e133a
Daria <daria.z@mail.ru>
2017-12-04 20:36:49 +0200

    paginatorMore

--- assets/js/listing.js
+++ assets/js/listing.js
@@ -1,11 +1,18 @@
 var  personPerPage=20;
 var  pages= [];
+var currentPage=1;
+var currentUsers=[];
+
 
 var app = new Vue({
     el: '#UserList',
     data: {
         users: Persons,
-         pages:pages
+        pages:pages,       
+        currentPage:0,
+        personPerPage:20,
+        currentUsers:currentUsers,
+        counter: 0        
     },
     methods: {
         colvoPages: function (){
@@ -17,6 +24,24 @@ var app = new Vue({
             }
              return pages;
         },
-    }, //methods
+        getCurrentusers:function(){
+          this.currentUsers=Persons.slice(this.currentPage, (this.currentPage + this.personPerPage));  
+        console.log(this.currentUsers);
+        },
+        count: function(){
+            
+            if (this.currentPage>=this.colvoPages()){
+              this.currentPage=0;                
+            }
+            else {
+                 this.currentPage++;            
+            } 
+        }
+    }, 
+    computed:{
+        currentUsers:this.getCurrentusers();
+    }
 }) //Vue
 pages=app.pagess();
+app.currentUsers=app.getCurrentusers();
+currentUsers=app.getCurrentusers();
