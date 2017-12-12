e1ebc0562703f96a477499a26d2c13d8b2ac7a61
Arteom Trifaniuck <fomorden@gmail.com>
2017-12-12 20:47:27 +0200

    Added last changes to js + added constraint for unique description

--- addInterest.js
+++ addInterest.js
@@ -1,17 +1,5 @@
-var persons=[];
-var convertedPersons;
 var express = require('express')
 var app = express()
-
-/*
-db.run(`INSERT INTO Interest(description) VALUES(?)`, ['description'], function(err) {
-    if (err) {
-      return console.log(err.message);
-    }
-    // get the last insert id
-    console.log(`A row has been inserted with rowid ${this.lastID}`);
-  });
-
 
 const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('user-store.db', (err) => {
@@ -20,10 +8,17 @@ let db = new sqlite3.Database('user-store.db', (err) => {
   }
   console.log('Connected to the in-memory SQlite database.');
 });
-*/
 
-app.get('/p/:tagId', function(req, res) {
-  res.send("tagId is set to " + req.params.tagId);
+console.log('Server is up.');
+app.get('/interest/new', function (req, res) {
+  db.run(`INSERT INTO Interest(description) VALUES(?)`, [req.query.desc], function (err) {
+    if (err) {
+      res.send(err.message)
+      return console.log(err.message);
+    }
+    console.log(`A row has been inserted with rowid ${this.lastID}`);
+    res.send("description added with: " + req.query.desc);
+  });
 });
 
-
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
