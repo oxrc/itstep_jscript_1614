ba6e8ea22a68f2e328ae0bf20fa5458c658681e5
Timur Shustov <timur.shustov@mail.ru>
2018-01-18 18:53:01 +0200

    Updated readuser

--- readuser.js
+++ readuser.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var app = express();
 
 const sqlite3 = require('sqlite3').verbose();
@@ -23,4 +23,33 @@ db.get(sql, [id], (err, row) => {
         console.log(`No user found with the id ${id}`);
 });
 db.close();
+app.listen(3000, () => console.log('Listening on 300'))
+*/
+var express = require('express')
+var app = express()
+
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('user-store.db', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+});
+
+console.log('Server is up.');
+app.get('/user/read', function (req, res) {
+    db.run(`SELECT from Person WHERE id =` + req.query.id, function (err) {
+        if (err) {
+            status = {
+                status: "error",
+                error_text: err.message
+            }
+            res.json(status);
+            return console.log(err.message);
+        }
+        console.log(`A row ${req.query.id}`);
+    });
+
+});
+
 app.listen(3000, () => console.log('Listening on 300'))
